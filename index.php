53bf07996892b44fc26022abca475c244e84a2ed
Jan Elznic <jan@elznic.com>
2021-01-08 17:34:39 +0100

    CSS styles

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
 <html>
 <head>
   <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+  <link rel="stylesheet" type="text/css" href="styles.css">
   <title><?php echo $__title; ?></title>
 </head>
 
