c1fb06d631f3692478644370ebe9eee6b02662c0
Jan Elznic <jan@elznic.com>
2021-01-08 15:11:47 +0100

    Editor config, connection for DB, basic request

--- db.php
+++ db.php
@@ -5,9 +5,4 @@ $db_username = 'root';
 $db_password = 'aaa';
 
 $mysqli = mysqli_connect($db_server, $db_username, $db_password, $db_name);
-
-$result = $mysqli->query("SELECT username FROM users LIMIT 10");
-$row = $result->fetch_array(MYSQLI_ASSOC);
-
-$data = $row;
 ?>
--- index.php
+++ index.php
@@ -2,14 +2,33 @@
 <?php require_once('db.php'); ?>
 <html>
 <head>
-	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
-	<title><?php echo $__title; ?></title>
+  <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+  <title><?php echo $__title; ?></title>
 </head>
 
 <body>
 <h1><?php echo $__title; ?></h1>
 
+<?php
+$result = $mysqli->query("SELECT username FROM users");
+$users = $result->fetch_array(MYSQLI_ASSOC);
 
+$data = $users;
+?>
+
+<table>
+  <thead>
+    <tr>
+      <th colspan="2">Users</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td></td>
+      <td></td>
+    </tr>
+  </tbody>
+</table>
 
 <pre style="background: grey; color: white;">
 <?php var_dump($data); ?>
